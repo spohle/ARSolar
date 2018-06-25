dd78b53083181b75c21de7829fd232d58d189ab3
Pohle, Sven <spohle@stage-sound.activision.com>
2018-06-25 10:28:46 -0700

    Initial Commit

---
+++ ARSolar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARSolar
+//
+//  Created by Pohle, Sven on 6/25/18.
+//  Copyright © 2018 Pohle, Sven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
